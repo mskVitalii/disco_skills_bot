d6daa0e22bcfd43cf8fc8105036ab6904317b17d
mskKote <msk.vitaly@gmail.com>
2026-07-01 14:27:46 +0200

    fix migrations

--- migrations/models/1_20260628000000_spec3_stats_ideas.py
+++ migrations/models/1_20260628000000_spec3_stats_ideas.py
@@ -1,5 +1,7 @@
 from tortoise import BaseDBAsyncClient
 
+RUN_IN_TRANSACTION = True
+
 
 async def upgrade(db: BaseDBAsyncClient) -> str:
     return """
@@ -16,12 +18,44 @@ async def upgrade(db: BaseDBAsyncClient) -> str:
             "idea" TEXT NOT NULL,
             "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
             "user_id" INT NOT NULL REFERENCES "users" ("id") ON DELETE CASCADE
-        );
-    """
+        );"""
 
 
 async def downgrade(db: BaseDBAsyncClient) -> str:
     return """
         DROP TABLE IF EXISTS "user_major_ideas";
-        DROP TABLE IF EXISTS "skill_invocation_stats";
-    """
+        DROP TABLE IF EXISTS "skill_invocation_stats";"""
+
+
+MODELS_STATE = (
+    "eJztW21P4zgQ/itRvhwncQhaunCn00nlbbe30K5ouVstQpGbuKmPxO7GDi9a8d/Pdl6dbd"
+    "qQJgVKvgCxzYz9zNjzzMT5obvEgg7duaLQ0//QfuhgNuO/w2Z9W9MxcGHSEgzkzQyMHdnu"
+    "8wY5EGELPkDK265v+KMLMLChxR+x7zi8AYwp84DJeMsEOBTyptmtMUHQsaTmSBGyhDQfo+"
+    "++eGaeL4ZacAJ8hyXiAnVWMkK0h5OK5FtjwySO7+JErkVMPg2E7USSDTH0AEvLkrMy2ONM"
+    "zqiH2ZmcJu8xCRbLQJhROWtbjPittbd/sH/Y/rB/yIfIKcQtB09y9tT00IwhghO9s0c2JT"
+    "jWwkXqwZwT7YEOOYf+SH96mr+ASQhjgn3LzbSQFsm0WICBVFOCP4MOtD3gGnUZIqNgiUUi"
+    "EapJjpC93Cq/t1rt9kFrt/3hsLN/cNA53I3N83NXRXY66n0UphKQc2cPtkhkuwRjsWnk32"
+    "mAY7DyEY6GpCCOzJJGOC2+FLzHU+DlguuCB8OB2GZT4eedTlHguJAFwP3TvTz+1L3c4gJ/"
+    "VeHrh12toE9FcoI8yowCWOr6MiTnOqsqf8PBNKeAZbd9Oa+ci2VK/Hvc9IgaPPyhu2WeGu"
+    "7o5+OrKCiHMCEOBDgP4qJojbmYRXANBuei26X0uxPglwGvf3VxdHq5tSedlw9CLAdT04Ni"
+    "QQZgZZ0W+IwYmNw/x48VpaWAPuG9DLkw15m5CmuAncfQH4oib4Vyd6I/9NQSDWClwnSOcU"
+    "a9i9PhqHvxRbHQSXd0KnpasvUx07r1IXPKxEK0f3ujT5p41L4N+qdi1IxQZntSYzJu9I0b"
+    "9iZvkYFdDUZsyKaSqEoKMwbm7T3wLEMhQIlz0FvkOPx0vQupbFVnWqmdFc717PMldIBcXv"
+    "n9leLiQ7HGc7FEPXLTSGTiOgkkFgIOsTcSjRO5tEIoBI5BGWAbiYT0iR6+I6YUOGSAFYLF"
+    "Bf8RjwdoCDYSFrFdLsQSe3yFuYAoB0smbRIKQ2Fd6CFzqhdJmcOh26mkGcRNTda8GVnzHf"
+    "Ro6KV1UOeU+A3PQcRuqgnEUHTNAO7t7lYLIBeYC6Dsy1BighnEpfnwUvKbiC8F5N/DQX/V"
+    "/OIK895rC5lsW3MQZTcLUBT6FCobgbd10f2axfX4fHCU5ahCwFFl7LRgeAnJTJHwkvCeOL"
+    "ykWN664ov+58THpgBG831k7Ygf+3+pNZcqQ85857q66p2s6lxy+kLQAqeS/tMOsp60r8h/"
+    "KxRw1Frg+ukWXzD3Hhjk1cfd4XH3RGZnHriPrS+nFhZsVJzPiAeRjT/DR4l2jwMNsAmr4W"
+    "gLqdlqpW3EnLpqrrHsmsNLp+rw0lkQXjo/h5emitVUsZoq1oIqVvY9T40F9ZT4UvZ7PSla"
+    "1aU/zMPJRhYxArbX5w+rVjBSkorTzEhxhmoaMd5NPeNV1DOyFd9XSjBD71krxXxGnXgGvG"
+    "oy6ch9K4QumNtLQLf08FmJo8uY5kJK+blRa9xM6Sh1uo/gQ+6BUA1DH51+HS0uXcQc5nzQ"
+    "/xgNz9YzVEoSvPvgzGXGp7w0RqbS+rGPHIYw3REVl0xmXxT4OcrfVxEpbYkIBoPIia7ZFP"
+    "O0v19bxHfSwlOhGtI+t04wX9N7vBDTpL0bmvYqtK6OAK4oeNuV4izbrO/kUeQ39QKFdZpT"
+    "5FgcnrVz/bdUMVAvUBT9WkC5cqF8NmBkLpw0tYO11w70IePJItWkJTS+Y1zudaYmTaLxBE"
+    "fADS1t4hFXEyb7hWp8/uI2gvROuiOzz+bF1gu/2ELh9qoj1kay31OS3DDTDWWmzQuZ0gSr"
+    "klBRx12ZzDXoorxEvTmtEpPszfGGmbyytxoNt1gbtwj2QkVfKy6oCq/pG7uXvp7pRCfOAi"
+    "TbpWCMJb/x0NPE65oLItfx8ZnaeTcrx+F5n50UCcY5n6vEETmYJIoHJB/uNGF5/QWDEQf0"
+    "lmpTcq+RCYNYg8CcatJGGqKaMNMtp4Ez6EkW2BQImiC+eUHcJP7SeyG7pWCMJb/xeNQE8W"
+    "qS7jLHbd0h/+l/+x9HZw=="
+)
--- migrations/models/4_20260630000000_raw_messages.py
+++ migrations/models/4_20260630000000_raw_messages.py
@@ -1,5 +1,7 @@
 from tortoise import BaseDBAsyncClient
 
+RUN_IN_TRANSACTION = True
+
 
 async def upgrade(db: BaseDBAsyncClient) -> str:
     return """
@@ -16,11 +18,46 @@ async def upgrade(db: BaseDBAsyncClient) -> str:
         );
         CREATE INDEX IF NOT EXISTS "idx_raw_messages_telegram_id" ON "raw_messages" ("telegram_id");
         CREATE INDEX IF NOT EXISTS "idx_raw_messages_chat_id" ON "raw_messages" ("chat_id");
-        CREATE INDEX IF NOT EXISTS "idx_raw_messages_created_at" ON "raw_messages" ("created_at");
-    """
+        CREATE INDEX IF NOT EXISTS "idx_raw_messages_created_at" ON "raw_messages" ("created_at");"""
 
 
 async def downgrade(db: BaseDBAsyncClient) -> str:
     return """
-        DROP TABLE IF EXISTS "raw_messages";
-    """
+        DROP TABLE IF EXISTS "raw_messages";"""
+
+
+MODELS_STATE = (
+    "eJztXG1P4zgQ/itRvhwncQgKXbjT6aTytttb2q6g3K0WochN3NZHYncThxet+O9nO6/ONm"
+    "1Ik0KDv+wSx8zYz4w9M09sfugOsaDt7Vx70NX/0H7oYDZj/4fN+ramY+DApCXoyJopGNmi"
+    "3WcNoiPCFnyEHmu7uWWPDsBgAi32iH3bZg1g5FEXmJS1jIHtQdY0uzPGCNqW0BwpQhaX5m"
+    "P03efP1PV5VwuOgW/TRFygzkp68PZwUJF8a2SYxPYdnMi1iMmGgfAkkTSBGLqApmWJURn0"
+    "aSZG1MX0XAyTvTEJ5tNAmHpi1BPe47fW3sHhwdH+h4Mj1kUMIW45fBaj90wXzSgiONE7e6"
+    "JTgmMtTKQejDnRHugQY+gP9efn+RMYhzAm2LecTAtpkUyLBShINSX4U2jDiQscoy5DZBQs"
+    "sUgkQjbJMZost8rvrdb+/mFrd//DUfvg8LB9tBub5+dXFdnpuPuRm4pDzpw9WCKR7RKM+a"
+    "IRP6cBjsHKRzjqkoI4Mksa4bT4UvCeTIGbC64DHg0b4gmdcj9vt4sCx4QsAO6fzuXJp87l"
+    "FhP4qwxfP3zVCt7JSI6R61GjAJa6vgzJuc4qy284mOYU0OyyL+eVc7FMiX+Pix55Bgt/6H"
+    "6Zp4Yr+uX4SgrKIUyIDQHOg7goWiMmZhFcg8EFf+143nc7wC8DXv+6d3x2ubUnnJd1QjQH"
+    "U9OFfEIGoGWdFviUGJg8vMSPJaWlgD5lbylyYK4zMxXWANtPoT8URd4K5e5EP+ipKRrASo"
+    "XpHOMMu72zq2Gn90Wy0GlneMbftETrU6Z160Nml4mFaP92h580/qh9G/TPeK8Z8ejEFRqT"
+    "fsNvzLC3eZMM7GpQMoF0KhJVkcKMgHn3AFzLkBKgxDm8O2TbbHe9D1PZqva0UisrHOv550"
+    "toAzG98usrlYtf8Tle8CnqkZtGIhPXSSCxELDJpJFonIqpFUIhcAyPAtpIJIRPdPE9MYXA"
+    "KwpoIVgc8B9xWYCGoJGw8OXS41PsshnmAiJtLJmyiSsMhXWgi8ypXqRkDrtup4pmEDepqr"
+    "kZVfM9dL3QS+tInVPiG16D8NVUE4ih6JoB3NvdrRZAJjAXQPEukxITTCEunQ8vTX4T8aWA"
+    "/Ptq0F+1vrjG7O2NhUy6rdnIo7cLUOT6pFQ2Am+r1/maxfXkYnCczVG5gOPKstOC4SVMZo"
+    "qElyTvicNLKstbV3zR/xz72OTAaL6PrB3+z8FfMudSZciZ71zX193TVZ1LDJ8LWuBUwn/2"
+    "g6on7Svi1woFHJkLXH+6xSbMvAcGdfVJ5+qkcyqqMxc8xNYXQwsJGxnnc+JCNMGf4ZNAu8"
+    "uABtiE1eRoC1Oz1ahtRO26ONdYds3hpV11eGkvCC/tOeGlMo5wHoqKIlQUoa4oQl1RhPMp"
+    "wuxHtBq/VqTEl7Lf26l/q+ZVMYvVjWSIglS6zx5WpYdSkorn8JHiTB5vxHgrsuhNkEVZOv"
+    "2NZu+h96w1f38BCT8DbjU0ReS+FUIXjO01oFu6+axUAImY5kDPY/tGrXEzpaPU7j6Ej7kb"
+    "QjXlz/Ds63AxLxTnMBeD/seoe5YsklOS4MMSy1xmbMhLY2SKMxn5yKYIezuczsrQJkWBn6"
+    "P8fTF0aUtEMBhEDHTNppin/f3aIj7wF+4K9dEHOZreI5Wgyt6Glr1SWldHAJcUbDYNn802"
+    "69t5JPmKL5CyTnOKbIvBs/Zcf5MYg0vw0AuT5iKMQar7doox4FVMGPuyjMGNdPCejeomZt"
+    "uDh2Tvvm0Yu7CRofz1bnpUGEvUXQ91qL5WdNVNmqoOXtVenKmaLEJAyKxpP8jqqNtrKz6N"
+    "0Mo/jND66SwChY9VEshy4HosX8NuKHs58j2EmfvwJBZDwYbVtxvkK2v4PquImQYRM2s9FC"
+    "pfYCh6W1+68iBd2zcyFz7U5+W1f17WryhhjqUJS2jMUxxAkakJk2gsinC4oaWNXeJo3GS/"
+    "eBobP78NIAgMb0d8oFQHS1/5YCkKl1cdCV0k+z1lIipGNihGqjN7lXDwlYSKutKS1DXkon"
+    "mJfHNZTkyyN7dVZvLGDr6p3GJtuUWwFiriOBccHFoTzfna1yPtaMdZgOR+KRhjyRseelS8"
+    "rvmb+U28faZW3u3KcXjen30oEoxz/lxEHJGDQaK4Q/KHM1RYXj9hMGSA3nnalDxoZEwh1i"
+    "Awp5qwkYY8jZvpjqWBM+iKLFARBCqINy+Im8RfenVgtxSMseQNj0cqiFdTdJfZbusO+c//"
+    "A2omKkA="
+)
